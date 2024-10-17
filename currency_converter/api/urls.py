39df3263eaf3b994f74a28bd87f958b4e8ed3f65
Israel David <israeldiv100@gmail.com>
2024-10-16 21:41:43 -0300

    creating homepage
    created the function in views to get the currencies
    
    first steps of the html

--- currency_converter/api/urls.py
+++ currency_converter/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import convert_currency
+from .views import convert_currency, currencies
 
 urlpatterns = [
-    path('convert/', convert_currency, name='convert_currency'),  # Define a rota para 'convert'
+    path('convert/', convert_currency, name='convert_currency'), 
+    path('', currencies, name='currency converter')
 ]
